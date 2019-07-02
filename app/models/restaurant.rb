a2472b80377afb44c378e1cc131ddc48ff2b7be5
Dennis Wang <dwang0816@gmail.com>
2019-07-02 10:57:02 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,24 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    @@all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    reviews.all.select do |review|
+    review += reviews.rating
+    end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,13 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+res1 = Restaurant.new('pelicana')
+res2 = Restaurant.new('kfc')
+res3 = Restaurant.new('popeyes')
+cus1 = Customer.new('dennis', 'wang')
+cus2 = Customer.new('gina', 'lee')
+cus3 = Customer.new('marco','benzo')
 
+cus1.add_review('kfc', 'its good', 3)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
