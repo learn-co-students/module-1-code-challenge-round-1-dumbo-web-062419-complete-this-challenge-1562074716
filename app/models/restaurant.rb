882750b71281f98c7e53b22465437dc8763ab0d6
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-07-02 10:58:28 -0400

    finish

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
   attr_reader :name
+  attr_accessor :rating
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @rating =  
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
+  def customers
+    #come back
+  end
+
+  def reviews
+    #come back
+  end
+
+  def average_star_rating
+    #come back
+  end
+
+  def longest_review
+    #come back
+  end
+
+  def self.find_by_name(name)
+    #come back
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,38 @@
 class Review
+
+    attr_reader :restaurant, :customer
+    attr_accessor :content
+
+    @@all = []
+
+    def initialize(customer, restaurant)
+        @customer = customer
+        @restaurant = restaurant
+        @content = ''
+        @@all << self 
+    end
+
+    def customer 
+        #come back
+    end
+
+    def restaurant
+        #come back
+    end
+
+    def rating
+        #come back
+    end 
+
+    def content
+        #come back
+    end
+
+    def self.all
+        @all
+    end
+
+        
   
 end
 
