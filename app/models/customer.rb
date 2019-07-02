ae8c50b86ea2b27fba802f8f4fe2ffca403101a2
github username <github email address>
2019-07-02 10:56:27 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,43 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_reviews(restaurant, content, rating)
+    Review.new(self, restaurant,content,rating)
+  end
+
+  def num_reviews
+    # total number of reviews for this customer
+   Review.all.select {|reviews| reviews.customer == self}.length
+  end
+
+  def my_helper
+    Review.all.select {|reviews| reviews.customer == self}
+  end
+
+
+  def restaurants
+    #only return the same rest once - uniq
+    my_helper.map {|restaurant| restaurant.restaurant}.uniq
+
+  end
+    
+
 end
+
+#one customer can have many reviews
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,43 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+
+    def reviews
+      #all reviews for this restaurant
+      Review.all.select {|review| review.restaurant == self}
+    end
+
+    def customers
+      reviews.map {|review| review.customer}.uniq
+    end
+
+    def average_star_rating
+      #average for restaurant devide total number by number of reviews
+
+      tot = reviews.map {|review| review.rating}.sum
+      tot / reviews.map {|review| review.customer}.length
+
+    end
+
+    def longest_review
+
+      #the longest review for a rest
+
+     longest_one = reviews.map {|review| review.content}.max
+    end
+
+
 end
+
+#a restaurant can have many reviews
