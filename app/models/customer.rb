00d279bce31edfd87ad5a6f87bbb4d9bf52e1f03
Your Name <you@example.com>
2019-07-02 10:57:13 -0400

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,43 @@
 class Customer
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :restaurant
+  attr_accessor :content, :rating
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+
+  end
+
+
+  def add_review(restaurant, content, rating)
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+    @@all << self
+  end
+
+  #Returns the total number of reviews that a customer has authored
+  def num_reviews
+    @@all.select do |review|
+      review.restaurant == self
+
+    end
+  end
+
+  def restaurants
+    #Returns a **unique** array of all restaurants a customer has reviewed
+
+  end
+
+
+
+  
+
+  def self.all 
+    @@all
   end
 
   def full_name
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,36 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def customers
+    #Returns a **unique** list of all customers who have reviewed a particular restaurant.
+  end
+
+
+ def reviews
+  #returns an array of all reviews for that restaurant
+  end
+
+  def average_star_rating
+    #returns the average star rating for a restaurant based on its reviews
+  end
+
+  def longest_review
+  #returns the longest review content for a given restaurant
+
+  end
+
+
+  def self.all 
+    @@all
+  end
+
+
 
 end
