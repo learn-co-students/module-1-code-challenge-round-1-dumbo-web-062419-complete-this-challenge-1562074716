175e2fd405c9eebfc525988cc1ec9e3fb337fb7e
TeenageGrampa <danielhvoigt@gmail.com>
2019-07-02 10:56:15 -0400

    I think I finisher right on time

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
+  @@all_fullnames = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all_fullnames << "#{first_name} #{last_name}"
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all_names
+    @@all_fullnames
+  end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    Review.all.select{|review| review.customer == self}.uniq
+  end
+
+  def add_review(title, restaurant, content, rating)
+    Review.new(title, restaurant, content, rating, self)
+  end
+
+  def self.find_by_name(full_name)
+    @@all_fullnames.find{|customer| customer == full_name}
+  end
+
+  def self.find_by_first_name(name)
+    @@all.find{|customer| customer.first_name == name}
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+restaurant1 = Restaurant.new('kfc')
+restaurant2 = Restaurant.new('subway')
+restaurant3 = Restaurant.new('chilis')
+customer3 = Customer.new('ariah', 'noetzel')
+customer1 = Customer.new('dan', 'voigt')
+customer2 = Customer.new('jon', 'smith')
+customer1.add_review('was bad',restaurant1, 'bad', 2)
+customer1.add_review('delish', restaurant2, 'good', 10)
+customer2.add_review('alright',restaurant1, 'ok', 5)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
