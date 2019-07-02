aaaebd48f63f4f1b9312275747684fc230e95d2b
github username <github email address>
2019-07-02 10:57:06 -0400

    Almost done :(

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,45 @@
 class Restaurant
+
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |restaurant| restaurant.name == name}
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def customers
+    all_reviews.select { |review| }
+  end
+
+  def average_star_rating
+    total_stars = 0
+    total_reviews = reviews.length
+
+    reviews.each do |review|
+      total_stars += review.rating
+    end
+
+    total_stars / total_reviews
+  end
+
+  def longest_review
+    reviews.sort_by! { |review| review.content.length }
+    reviews[-1]
   end
 
 end
