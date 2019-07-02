87fd00fad449135d7eeeb890efd9953293e48202
Rashad <rashad.walcott15@gmail.com>
2019-07-02 10:56:07 -0400

    Froze...:-(

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,58 @@
 class Customer
   attr_reader :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+  
+  def self.find_by_name(name)
+    #find first string of full name
+    #first that matches
+    Review.all.find do |name|
+      
+      
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    #returns an array containing all customers
+    #with that first name
+
+
+  end
+
+  def self.all_names
+    #return an array of all customer full names
+
+  end
+
+  def add_review(restaurant,content,rating)
+    #creates a new review and associates
+    #with that customer and restaurant
+
+  end
+
+  def num_reviews
+    #returns the total number of reviews that
+    #a customer has authored
+
+
+  end
+
+  def restaurants
+    #returns a unique array of all restaurants
+    #a customer has reviewed
+
+
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,29 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Rashad","Walcott")
+c2 = Customer.new("Cindy","Harryram")
+c3 = Customer.new("Monica","Venture")
+c4 = Customer.new("John","Walcott")
+c5 = Customer.new("Jehan","Cortez")
+
+
+r1 = Restaurant.new("Da Nugget Spot")
+r2 = Restaurant.new("Tu Casa")
+r3 = Restaurant.new("Lemon and Ginger Grass")
+r4 = Restaurant.new("Hurricane Grill")
+r5 = Restaurant.new("Chicken Spot")
+
+
+r1 = Review.new(c2.full_name,r3,"Food was good", 8)
+r2 = Review.new(c2,r3,"Not worth it", 3)
+r3 = Review.new(c2,r3,"Amazing", 9)
+r4 = Review.new(c2,r3,"Garbage", 1)
+r5 = Review.new(c2,r3,"So so", 6)
+
+
+
+
 binding.pry
+
 0 #leave this here to ensure binding.pry isn't the last line
