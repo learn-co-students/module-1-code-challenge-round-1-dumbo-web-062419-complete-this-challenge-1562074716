ef9c3acdacfd58be6fbdff3500b3369ed0ca660c
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-07-02 10:48:20 -0400

    finished code challenge 1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,39 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+  	@@all
+  end
+
+  def reviews
+    # - returns an array of all reviews for that restaurant
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    # - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+    reviews.map{|my_review| my_review.customer}.uniq
+  end
+
+  def average_star_rating
+    # - returns the average star rating for a restaurant based on its reviews
+    review_array = reviews
+    review_array.sum{|review| review.rating} / review_array.length
+  end
+
+  def longest_review
+    # - returns the longest review content for a given restaurant
+    reviews.max_by{|review| review.content.length}
+  end
+
+	def self.find_by_name(name)
+		# - given a string of restaurant name, returns the first restaurant that matches
+		self.all.find{|restaurant| restaurant.name == name}
+	end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,38 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("john", "green")
+customer2 = Customer.new("john", "green")
+customer3 = Customer.new("billy", "joel")
+customer4 = Customer.new("sally", "mae")
+
+restaurant1 = Restaurant.new("cluckin' chicken")
+restaurant2 = Restaurant.new("bangin' burgers")
+restaurant3 = Restaurant.new("phenomenol' french fries")
+restaurant4 = Restaurant.new("cluckin' chicken")
+restaurant5 = Restaurant.new("restaurant 5")
+restaurant6 = Restaurant.new("restaurant 6")
+
+review1 = Review.new(customer1, restaurant1, 5, "cluckin' chicken is mother cluckin' good!")
+review2 = Review.new(customer1, restaurant2, 4, "bangin burgers are bangin fo sho fo sho!")
+review3 = Review.new(customer1, restaurant3, 5, "phenomenol french fries are phenom nom nom")
+review4 = Review.new(customer2, restaurant1, 1, "pldldld")
+review5 = Review.new(customer3, restaurant1, 2, "cluckin' chicken is mother cluckin' delicious!!!")
+review6 = Review.new(customer4, restaurant1, 3, "short review")
+customer1.add_review(restaurant3, 1, "you know what, its actually not that great")
+
+reviews = customer1.reviews
+num_reviews = customer1.num_reviews
+restaurants = customer1.restaurants
+found_customer = Customer.find_by_name("freddy mac")
+found_customers = Customer.find_all_by_first_name("john")
+all_names = Customer.all_names
+cluckin = restaurant1.reviews
+mother_cluckers = restaurant1.customers
+avg_star = restaurant1.average_star_rating
+longest = restaurant1.longest_review
+rest = Restaurant.find_by_name("cluckin' chicken") # => first cluckin' chicken
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
